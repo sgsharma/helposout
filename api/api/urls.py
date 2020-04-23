3c034f0b78da80914622e642c1c4bd34b0238245
sgsharma <sasha.s.sharma@gmail.com>
2020-04-22 23:11:30 -0700

    updated urls to use path instead

--- api/api/urls.py
+++ api/api/urls.py
@@ -1,9 +1,11 @@
-from django.conf.urls import url, include
-from django.views.generic import TemplateView
 from django.conf.urls import include, url
+from django.contrib import admin
+from django.urls import include, path, re_path
+from django.views.generic import TemplateView
 from rest_framework import routers
 
-from .views import JobViewSet, OrganizationViewSet, PartnerViewSet, ApplicantViewSet
+from .views import (ApplicantViewSet, JobViewSet, OrganizationViewSet,
+                    PartnerViewSet)
 
 router = routers.DefaultRouter(trailing_slash=False)
 router.register(r'jobs/?', JobViewSet)
@@ -12,6 +14,7 @@ router.register(r'partners/?', PartnerViewSet)
 router.register(r'applicants/?', ApplicantViewSet)
 
 urlpatterns = [
-    url(r'^api/v1/', include(router.urls)),
-    url(r'^', TemplateView.as_view(template_name="index.html")),
+    path('admin/', admin.site.urls),
+    re_path(r'^api/v1/', include(router.urls)),
+    re_path(r'^', TemplateView.as_view(template_name="index.html")),
 ]
