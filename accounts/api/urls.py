033b0778757e640bf67423475462071fcc513606
sgsharma <sasha.s.sharma@gmail.com>
2020-07-07 23:29:30 -0700

    removed knox urls from accounts

--- accounts/api/urls.py
+++ accounts/api/urls.py
@@ -19,12 +19,12 @@ org_highlight = OrganizationViewSet.as_view({
 }, renderer_classes=[renderers.StaticHTMLRenderer])
 
 urlpatterns = [
-    path('', include('knox.urls')),
+    # path('', include('knox.urls')),
     path('user', UserAPIView.as_view()),
     re_path(r'register/?', RegisterAPIView.as_view()),
-    path('login', LoginAPIView.as_view()),
-    path('logout', LogoutView.as_view(), name='knox_logout'),
-    path('orgs/', org_list, name='org-list'),
-    path('orgs/<int:pk>/', org_detail, name='org-detail'),
-    path('orgs/<int:pk>/highlight/', org_highlight, name='org-highlight'),
+    re_path(r'login/?', LoginAPIView.as_view()),
+    re_path(r'logout/?', LogoutView.as_view(), name='knox_logout'),
+    re_path(r'orgs/?', org_list, name='org-list'),
+    re_path(r'orgs/<int:pk>/?', org_detail, name='org-detail'),
+    re_path(r'orgs/<int:pk>/highlight/?', org_highlight, name='org-highlight'),
 ]
